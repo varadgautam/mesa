52505950cbe5824e95bfa0e3d9318138f6c34377
Ben Widawsky <ben@bwidawsk.net>
2016-10-24 22:52:06 -0700

    i965/miptree: Add a return for updating of winsys
    There is nothing particularly useful to do currently if the update
    fails, but there is no point carrying on either. As a result, this has a
    behavior change.
    
    v2: Make the return type a bool (Topi)
    
    v3: Don't leak the bo if update_winsys_renderbuffer fails. (Jason)
    
    Signed-off-by: Ben Widawsky <benjamin.widawsky@intel.com>
    Acked-by: Daniel Stone <daniels@collabora.com>
    Reviewed-by: Topi Pohjolainen <topi.pohjolainen@intel.com> (v2)

--- src/mesa/drivers/dri/i965/intel_mipmap_tree.c
+++ src/mesa/drivers/dri/i965/intel_mipmap_tree.c
@@ -875,7 +875,7 @@ intel_miptree_create_for_image(struct brw_context *intel,
  * that will contain the actual rendering (which is lazily resolved to
  * irb->singlesample_mt).
  */
-void
+bool
 intel_update_winsys_renderbuffer_miptree(struct brw_context *intel,
                                          struct intel_renderbuffer *irb,
                                          drm_intel_bo *bo,
@@ -942,12 +942,12 @@ intel_update_winsys_renderbuffer_miptree(struct brw_context *intel,
          irb->mt = multisample_mt;
       }
    }
-   return;
+   return true;
 
 fail:
    intel_miptree_release(&irb->singlesample_mt);
    intel_miptree_release(&irb->mt);
-   return;
+   return false;
 }
 
 struct intel_mipmap_tree*

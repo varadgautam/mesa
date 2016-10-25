3c926f7dde8a6407a78af2b34de9bca3a11981c1
Ben Widawsky <ben@bwidawsk.net>
2016-10-24 23:00:02 -0700

    i965/miptree: Allocate mt earlier in update winsys
    Allows us to continue utilizing common miptree creation using __DRIimage
    without creating a new DRIimage (for the intel_process_dri2_buffer()
    case).
    
    This is a bit ugly, but I think it's the best one can do.
    
    Signed-off-by: Ben Widawsky <ben@bwidawsk.net>
    Acked-by: Daniel Stone <daniels@collabora.com>

--- src/mesa/drivers/dri/i965/intel_mipmap_tree.c
+++ src/mesa/drivers/dri/i965/intel_mipmap_tree.c
@@ -919,11 +919,10 @@ intel_miptree_create_for_image(struct brw_context *intel,
 bool
 intel_update_winsys_renderbuffer_miptree(struct brw_context *intel,
                                          struct intel_renderbuffer *irb,
-                                         drm_intel_bo *bo,
+                                         struct intel_mipmap_tree *singlesample_mt,
                                          uint32_t width, uint32_t height,
                                          uint32_t pitch)
 {
-   struct intel_mipmap_tree *singlesample_mt = NULL;
    struct intel_mipmap_tree *multisample_mt = NULL;
    struct gl_renderbuffer *rb = &irb->Base.Base;
    mesa_format format = rb->Format;
@@ -934,18 +933,7 @@ intel_update_winsys_renderbuffer_miptree(struct brw_context *intel,
     */
    assert(_mesa_get_format_base_format(format) == GL_RGB ||
           _mesa_get_format_base_format(format) == GL_RGBA);
-
-   singlesample_mt = intel_miptree_create_for_bo(intel,
-                                                 bo,
-                                                 format,
-                                                 0,
-                                                 width,
-                                                 height,
-                                                 1,
-                                                 pitch,
-                                                 MIPTREE_LAYOUT_FOR_SCANOUT);
-   if (!singlesample_mt)
-      goto fail;
+   assert(singlesample_mt);
 
    /* If this miptree is capable of supporting fast color clears, set
     * mcs_state appropriately to ensure that fast clears will occur.
@@ -985,7 +973,6 @@ intel_update_winsys_renderbuffer_miptree(struct brw_context *intel,
    return true;
 
 fail:
-   intel_miptree_release(&irb->singlesample_mt);
    intel_miptree_release(&irb->mt);
    return false;
 }
